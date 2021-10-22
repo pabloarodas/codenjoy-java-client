e03b3f3c6efbbe48b5ff345e2ddebd35fcf5ea28
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-22 12:49:14 +0300

    [clients][all] Fixed phase print style.

--- 2-build.bat
+++ 2-build.bat
@@ -1,9 +1,9 @@
 call 0-settings.bat
 
 echo off
-echo        [44;93m+-------------------------------------------------------------------------+[0m
-echo        [44;93m!                 Now we are building java client...                      ![0m
-echo        [44;93m+-------------------------------------------------------------------------+[0m
+echo        [44;93m+----------------------------------------------+[0m
+echo        [44;93m!      Now we are building java client...      ![0m
+echo        [44;93m+----------------------------------------------+[0m
 echo on
 
 call %MVNW% clean install -DskipTests=%SKIP_TESTS%
