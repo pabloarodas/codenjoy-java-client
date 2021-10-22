141e2c0a03c8dce8bb2988d4d5c9a4a6be99227f
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-22 14:11:07 +0300

    [clients][java] Refactoring. Moved install function into lib.bat.

--- 1-download-env.bat
+++ 1-download-env.bat
@@ -8,8 +8,7 @@ if "%SKIP_JDK_INSTALL%"=="true" ( goto :skip )
 if "%INSTALL_LOCALLY%"=="false" ( goto :skip )
 if "%INSTALL_LOCALLY%"=="" ( goto :skip )
 
-call :install jdk %ARCH_JDK% %ARCH_JDK_FOLDER%
-
+call lib.bat :install jdk %ARCH_JDK% %ARCH_JDK_FOLDER%
 call lib.bat :print_color %MVNW% -v
 
 call lib.bat :ask
@@ -23,16 +22,4 @@ goto :eof
 	call lib.bat :color SKIP_JDK_INSTALL=%SKIP_JDK_INSTALL%
 	echo on
 	call lib.bat :ask
-    goto :eof
-
-:install
-    cd %ROOT%
-    IF EXIST %TOOLS%\%~1.zip (
-        del /Q %TOOLS%\%~1.zip
-    )
-    powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('%~2','%TOOLS%\%~1.zip') }"
-    rd /S /Q %TOOLS%\..\.%~1
-    %ARCH% x -y -o%TOOLS%\.. %TOOLS%\%~1.zip
-    rename %TOOLS%\..\%~3 .%~1
-    cd %ROOT%
     goto :eof
--- lib.bat
+++ lib.bat
@@ -21,3 +21,15 @@ goto :eof
     echo on
     pause >nul
     goto :eof
+
+:install
+    cd %ROOT%
+    IF EXIST %TOOLS%\%~1.zip (
+        del /Q %TOOLS%\%~1.zip
+    )
+    powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('%~2','%TOOLS%\%~1.zip') }"
+    rd /S /Q %TOOLS%\..\.%~1
+    %ARCH% x -y -o%TOOLS%\.. %TOOLS%\%~1.zip
+    rename %TOOLS%\..\%~3 .%~1
+    cd %ROOT%
+    goto :eof
