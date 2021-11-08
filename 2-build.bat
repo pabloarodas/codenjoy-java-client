3f01fca509e3b01f1ec50be5d8d29f2cb6ee4f25
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-08 02:13:29 +0200

    [clients][java][pseudo][bat] Make build and launch methods identical.

--- 2-build.bat
+++ 2-build.bat
@@ -6,6 +6,8 @@ echo on
 
 call lib :print_color %MVNW% -v
 
+call %MVNW% clean install -DskipTests=%SKIP_TESTS%
+
 rem to create executable jar please uncomment lines
 rem call %MVNW% clean compile assembly:single -Pjar-with-dependencies -DskipTests=%SKIP_TESTS%
 rem echo F|xcopy /y %ROOT%\target\client-java-exec.jar %ROOT%\app.jar
