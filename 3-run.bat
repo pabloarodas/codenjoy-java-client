831e7641276897dd4164e602dcd9e11ae32fb0f3
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-17 00:06:37 +0200

    [clients][jvm] Made all jvm clients the same at the time of launching the bat file.

--- 3-run.bat
+++ 3-run.bat
@@ -4,10 +4,10 @@ echo off
 call lib :color Starting java client...
 echo on
 
-rem another way to run after 2-build.bat
-rem call %JAVA% -Dfile.encoding=UTF-8 -jar %ROOT%/app.jar "%GAME_TO_RUN%" "%BOARD_URL%"
+rem run jar
+call %JAVA% -Dfile.encoding=UTF-8 -jar %ROOT%/app.jar "%GAME_TO_RUN%" "%BOARD_URL%"
 
-rem another way to run without after 2-build.bat
-call %MVNW% clean compile exec:java -Dfile.encoding=UTF-8 -D"exec.mainClass"="com.codenjoy.dojo.JavaRunner" -D"exec.args"="%GAME_TO_RUN% %BOARD_URL%"
+rem build & run (without jar)
+rem call %MVNW% clean compile exec:java -Dfile.encoding=UTF-8 -D"exec.mainClass"="com.codenjoy.dojo.JavaRunner" -D"exec.args"="%GAME_TO_RUN% %BOARD_URL%"
 
 call lib :ask
