7da63c43aa5a3ada0d5a634f6b7eec10a9bbc83b
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-17 09:27:22 +0200

    [all][clifford][element] Changed sprites chars.

--- src/main/java/com/codenjoy/dojo/games/clifford/Element.java
+++ src/main/java/com/codenjoy/dojo/games/clifford/Element.java
@@ -26,6 +26,7 @@ package com.codenjoy.dojo.games.clifford;
 import com.codenjoy.dojo.services.printer.CharElement;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public enum Element implements CharElement {
 
@@ -68,149 +69,149 @@ public enum Element implements CharElement {
 
 /// Your hero
 
-    HERO_DIE('Ѡ',               "Your hero is dead. In the next tick, it will " +
+    HERO_DIE('O',               "Your hero is dead. In the next tick, it will " +
                                 "disappear and appear in a new location."),
 
-    HERO_LADDER('Y',            "Your hero is climbing the ladder."),
+    HERO_LADDER('A',            "Your hero is climbing the ladder."),
 
     HERO_LEFT('◄',              "Your hero runs to the left."),
 
     HERO_RIGHT('►',             "Your hero runs to the right."),
 
-    HERO_FALL(']',              "Your hero is falling."),
+    HERO_FALL('U',              "Your hero is falling."),
 
-    HERO_PIPE('{',              "Your hero is crawling along the pipe."),
+    HERO_PIPE('I',              "Your hero is crawling along the pipe."),
 
-    HERO_PIT('⍃',               "Your hero in the pit."),
+    HERO_PIT('E',               "Your hero in the pit."),
 
 /// Your hero in shadow mode
 
-    HERO_MASK_DIE('x',          "Your shadow-hero is dead. In the next tick, " +
+    HERO_MASK_DIE('o',          "Your shadow-hero is dead. In the next tick, " +
                                 "it will disappear and appear in a new location."),
 
-    HERO_MASK_LADDER('⍬',       "Your shadow-hero is climbing the ladder."),
+    HERO_MASK_LADDER('a',       "Your shadow-hero is climbing the ladder."),
 
-    HERO_MASK_LEFT('⊲',         "Your shadow-hero runs to the left."),
+    HERO_MASK_LEFT('h',         "Your shadow-hero runs to the left."),
 
-    HERO_MASK_RIGHT('⊳',        "Your shadow-hero runs to the right."),
+    HERO_MASK_RIGHT('w',        "Your shadow-hero runs to the right."),
 
-    HERO_MASK_FALL('⊅',         "Your shadow-hero is falling."),
+    HERO_MASK_FALL('u',         "Your shadow-hero is falling."),
 
-    HERO_MASK_PIPE('⋜',         "Your shadow-hero is crawling along the pipe."),
+    HERO_MASK_PIPE('i',         "Your shadow-hero is crawling along the pipe."),
 
-    HERO_MASK_PIT('ᐊ',          "Your shadow-hero in the pit."),
+    HERO_MASK_PIT('e',          "Your shadow-hero in the pit."),
 
 /// Other heroes
 
-    OTHER_HERO_DIE('Z',         "Other hero is dead. In the next tick, it will " +
+    OTHER_HERO_DIE('C',         "Other hero is dead. In the next tick, it will " +
                                 "disappear and appear in a new location."),
 
-    OTHER_HERO_LADDER('U',      "Other hero is climbing the ladder."),
+    OTHER_HERO_LADDER('D',      "Other hero is climbing the ladder."),
 
-    OTHER_HERO_LEFT(')',        "Other hero runs to the left."),
+    OTHER_HERO_LEFT('«',        "Other hero runs to the left."),
 
-    OTHER_HERO_RIGHT('(',       "Other hero runs to the right."),
+    OTHER_HERO_RIGHT('»',       "Other hero runs to the right."),
 
-    OTHER_HERO_FALL('⊐',        "Other hero is falling."),
+    OTHER_HERO_FALL('F',        "Other hero is falling."),
 
-    OTHER_HERO_PIPE('Э',        "Other hero is crawling along the pipe."),
+    OTHER_HERO_PIPE('J',        "Other hero is crawling along the pipe."),
 
-    OTHER_HERO_PIT('ᗉ',         "Other hero in the pit."),
+    OTHER_HERO_PIT('K',         "Other hero in the pit."),
 
 /// Other heroes in shadow mode
 
-    OTHER_HERO_MASK_DIE('⋈',    "Other shadow-hero is dead. In the next tick, " +
+    OTHER_HERO_MASK_DIE('c',    "Other shadow-hero is dead. In the next tick, " +
                                 "it will disappear and appear in a new location."),
 
-    OTHER_HERO_MASK_LADDER('⋕', "Other shadow-hero is climbing the ladder."),
+    OTHER_HERO_MASK_LADDER('d', "Other shadow-hero is climbing the ladder."),
 
-    OTHER_HERO_MASK_LEFT('⋊',   "Other shadow-hero runs to the left."),
+    OTHER_HERO_MASK_LEFT('Z',   "Other shadow-hero runs to the left."),
 
-    OTHER_HERO_MASK_RIGHT('⋉',  "Other shadow-hero runs to the right."),
+    OTHER_HERO_MASK_RIGHT('z',  "Other shadow-hero runs to the right."),
 
-    OTHER_HERO_MASK_FALL('⋣',   "Other shadow-hero is falling."),
+    OTHER_HERO_MASK_FALL('f',   "Other shadow-hero is falling."),
 
-    OTHER_HERO_MASK_PIPE('⊣',   "Other shadow-hero is crawling along the pipe."),
+    OTHER_HERO_MASK_PIPE('j',   "Other shadow-hero is crawling along the pipe."),
 
-    OTHER_HERO_MASK_PIT('ᗏ',    "Other shadow-hero in the pit."),
+    OTHER_HERO_MASK_PIT('k',    "Other shadow-hero in the pit."),
 
 /// Enemy heroes
 
-    ENEMY_HERO_DIE('Ž',         "Enemy hero is dead. In the next tick, it will " +
+    ENEMY_HERO_DIE('L',         "Enemy hero is dead. In the next tick, it will " +
                                 "disappear and appear in a new location."),
 
-    ENEMY_HERO_LADDER('Ǔ',      "Enemy hero is climbing the ladder."),
+    ENEMY_HERO_LADDER('N',      "Enemy hero is climbing the ladder."),
 
-    ENEMY_HERO_LEFT('❫',        "Enemy hero runs to the left."),
+    ENEMY_HERO_LEFT('P',        "Enemy hero runs to the left."),
 
-    ENEMY_HERO_RIGHT('❪',       "Enemy hero runs to the right."),
+    ENEMY_HERO_RIGHT('Q',       "Enemy hero runs to the right."),
 
-    ENEMY_HERO_FALL('⋥',        "Enemy hero is falling."),
+    ENEMY_HERO_FALL('R',        "Enemy hero is falling."),
 
-    ENEMY_HERO_PIPE('Ǯ',        "Enemy hero is crawling along the pipe."),
+    ENEMY_HERO_PIPE('T',        "Enemy hero is crawling along the pipe."),
 
-    ENEMY_HERO_PIT('⇇',         "Enemy hero in the pit."),
+    ENEMY_HERO_PIT('V',         "Enemy hero in the pit."),
 
 /// Enemy heroes in shadow mode
 
-    ENEMY_HERO_MASK_DIE('⧓',    "Enemy shadow-hero is dead. In the next tick, " +
+    ENEMY_HERO_MASK_DIE('l',    "Enemy shadow-hero is dead. In the next tick, " +
                                 "it will disappear and appear in a new location."),
 
-    ENEMY_HERO_MASK_LADDER('≠', "Enemy shadow-hero is climbing the ladder."),
+    ENEMY_HERO_MASK_LADDER('n', "Enemy shadow-hero is climbing the ladder."),
 
-    ENEMY_HERO_MASK_LEFT('⧒',   "Enemy shadow-hero runs to the left."),
+    ENEMY_HERO_MASK_LEFT('p',   "Enemy shadow-hero runs to the left."),
 
-    ENEMY_HERO_MASK_RIGHT('⧑',  "Enemy shadow-hero runs to the right."),
+    ENEMY_HERO_MASK_RIGHT('q',  "Enemy shadow-hero runs to the right."),
 
-    ENEMY_HERO_MASK_FALL('⌫',   "Enemy shadow-hero is falling."),
+    ENEMY_HERO_MASK_FALL('r',   "Enemy shadow-hero is falling."),
 
-    ENEMY_HERO_MASK_PIPE('❵',    "Enemy shadow-hero is crawling along the pipe."),
+    ENEMY_HERO_MASK_PIPE('t',    "Enemy shadow-hero is crawling along the pipe."),
 
-    ENEMY_HERO_MASK_PIT('⬱',    "Enemy shadow-hero in the pit."),
+    ENEMY_HERO_MASK_PIT('v',    "Enemy shadow-hero in the pit."),
 
 // Robbers (dummy AI-bots)
 
-    ROBBER_LADDER('Q',          "Robber is climbing the ladder."),
+    ROBBER_LADDER('X',          "Robber is climbing the ladder."),
 
-    ROBBER_LEFT('«',            "Robber runs to the left. Robber picks up the " +
+    ROBBER_LEFT(')',            "Robber runs to the left. Robber picks up the " +
                                 "nearest prey and hunts for it until it overtakes it."),
 
-    ROBBER_RIGHT('»',           "Robber runs to the right. Robber picks up the " +
+    ROBBER_RIGHT('(',           "Robber runs to the right. Robber picks up the " +
                                 "nearest prey and hunts for it until it overtakes it."),
 
-    ROBBER_FALL('‹',            "Robber is falling."),
+    ROBBER_FALL('x',            "Robber is falling."),
 
-    ROBBER_PIPE('<',            "Robber is crawling along the pipe."),
+    ROBBER_PIPE('Y',            "Robber is crawling along the pipe."),
 
-    ROBBER_PIT('⍇',             "Robber in the pit."),
+    ROBBER_PIT('y',             "Robber in the pit."),
 
 /// Doors and keys
 
-    OPENED_DOOR_GOLD('⍙',       "Opened golden gates. Can only be locked " +
+    OPENED_DOOR_GOLD('g',       "Opened golden gates. Can only be locked " +
                                 "with a golden key."),
 
-    OPENED_DOOR_SILVER('⍚',     "Opened silver gates. Can only be locked " +
+    OPENED_DOOR_SILVER('s',     "Opened silver gates. Can only be locked " +
                                 "with a silver key."),
 
-    OPENED_DOOR_BRONZE('⍜',     "Opened bronze gates. Can only be locked " +
+    OPENED_DOOR_BRONZE('b',     "Opened bronze gates. Can only be locked " +
                                 "with a bronze key."),
 
-    CLOSED_DOOR_GOLD('⍍',       "Closed golden gates. Can only be opened " +
+    CLOSED_DOOR_GOLD('G',       "Closed golden gates. Can only be opened " +
                                 "with a golden key."),
 
-    CLOSED_DOOR_SILVER('⌺',     "Closed silver gates. Can only be opened " +
+    CLOSED_DOOR_SILVER('S',     "Closed silver gates. Can only be opened " +
                                 "with a silver key."),
 
-    CLOSED_DOOR_BRONZE('⌼',     "Closed bronze gates. Can only be opened " +
+    CLOSED_DOOR_BRONZE('B',     "Closed bronze gates. Can only be opened " +
                                 "with a bronze key."),
 
-    KEY_GOLD('✦',               "Bronze key. Helps open/close golden gates. " +
+    KEY_GOLD('+',               "Bronze key. Helps open/close golden gates. " +
                                 "The key can only be used once."),
 
-    KEY_SILVER('✼',             "Silver key. Helps open/close silver gates. " +
+    KEY_SILVER('-',             "Silver key. Helps open/close silver gates. " +
                                 "The key can only be used once."),
 
-    KEY_BRONZE('⍟',             "Bronze key. Helps open/close bronze gates. " +
+    KEY_BRONZE('!',             "Bronze key. Helps open/close bronze gates. " +
                                 "The key can only be used once."),
 
 /// Other stuff
@@ -227,10 +228,10 @@ public enum Element implements CharElement {
     PIPE('~',                   "Pipe - the hero can also move along the " +
                                 "level along it, but only horizontally."),
 
-    BACKWAY('⊛',                "Back door - allows the hero to secretly " +
+    BACKWAY('W',                "Back door - allows the hero to secretly " +
                                 "move to another random place on the map."),
 
-    MASK_POTION('S',            "Disguise potion - endow the hero with " +
+    MASK_POTION('m',            "Disguise potion - endow the hero with " +
                                 "additional abilities. The hero goes into " +
                                 "shadow mode.");
 
