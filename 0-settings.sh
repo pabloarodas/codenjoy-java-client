a7c980ea3ef830c9fd0f2e08490e1e2d7aa7b857
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-19 14:09:07 +0200

    [clients][java][bash] Fixed mvnw settings. Maven will be downloaded in the .mvn folder inside client.

--- 0-settings.sh
+++ 0-settings.sh
@@ -18,22 +18,26 @@ eval_echo "ARCH=tar"
 # Set to true if you want to ignore jdk and maven installation on the system
 eval_echo "[[ \"$INSTALL_LOCALLY\" == \"\" ]] && INSTALL_LOCALLY=true"
 
-eval_echo "[[ \"$INSTALL_LOCALLY\" == "true" ]] && JAVA_HOME="
-eval_echo "[[ \"$INSTALL_LOCALLY\" == "true" ]] && MAVEN_HOME="
+eval_echo "[[ \"$INSTALL_LOCALLY\" == "true" ]] && export JAVA_HOME="
+eval_echo "[[ \"$INSTALL_LOCALLY\" == "true" ]] && export MAVEN_HOME="
 
-eval_echo "[[ \"$JAVA_HOME\" == \"\" ]]  && JAVA_HOME=$ROOT/.jdk"
+eval_echo "[[ \"$JAVA_HOME\" == \"\" ]]  && export JAVA_HOME=$ROOT/.jdk"
 eval_echo "[[ \"$MAVEN_HOME\" == \"\" ]] && NO_MAVEN=true"
-eval_echo "[[ \"$NO_MAVEN\" == "true" ]] && MAVEN_HOME=$ROOT/.mvn"
-eval_echo "[[ \"$NO_MAVEN\" == "true" ]] && MAVEN_OPTS=-Dmaven.repo.local=$MAVEN_HOME/repository"
+eval_echo "[[ \"$NO_MAVEN\" == "true" ]] && export MAVEN_HOME=$ROOT/.mvn"
+eval_echo "[[ \"$NO_MAVEN\" == "true" ]] && export MAVEN_USER_HOME=$ROOT/.mvn"
+eval_echo "[[ \"$NO_MAVEN\" == "true" ]] && export MAVEN_OPTS=-Dmaven.repo.local=$MAVEN_HOME/repository"
 
 eval_echo "MVNW=$ROOT/mvnw"
 eval_echo "chmod +x $MVNW"
+eval_echo "export MVNW_VERBOSE=false"
 eval_echo "JAVA=$JAVA_HOME/bin/java"
 eval_echo "export PATH=\"$JAVA_HOME/bin:$PATH\""
 
 color $COLOR4 "JAVA_HOME=$JAVA_HOME"
 color $COLOR4 "MAVEN_HOME=$MAVEN_HOME"
 color $COLOR4 "MAVEN_OPTS=$MAVEN_OPTS"
+color $COLOR4 "MAVEN_USER_HOME=$MAVEN_USER_HOME"
+color $COLOR4 "MVNW_VERBOSE=$MVNW_VERBOSE"
 echo
 
 eval_echo "ARCH_URL=https://builds.openlogic.com/downloadJDK/openlogic-openjdk/11.0.11%2B9/openlogic-openjdk-11.0.11%2B9-linux-x64.tar.gz"
