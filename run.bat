56edc062b386050bff2c4df84761a98099abbdf7
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-21 13:14:56 +0200

    [clients][java][bat] Fixed run.bat so it only contains the abstract part common to all clients. All specific step instructions are contained in stuff.bat.

--- run.bat
+++ run.bat
@@ -1,24 +1,29 @@
 @echo off
 
-rem red *;91
-rem green *;92
-rem yellow *;93
-rem blue *;94
-rem pink *;95
-rem light blue *;96
-rem purple *;97
-rem black background 40;*
-rem dark yellow background 43;*
-rem purple background 45;*
-rem blue background 44;*
-set CL_HEADER=43;93
-set CL_COMMAND=40;96
-set CL_QUESTION=45;97
-set CL_INFO=44;93
+:check_run_mode
+    if "%*"=="" (       
+        call :run_executable 
+    ) else (
+        call :run_library %*
+    )
+    goto :eof
 
-call :settings
+:run_executable
+    rem run run.bat as executable script
+    goto :init
 
-set OPTION=%1
+:run_library
+    rem run run.bat as library
+    set input=%* 
+    rem replace all ‘ to "
+    call set all=%%input:‘="%%
+    call %all%
+    goto :eof      
+
+:init
+    call :init_colors    
+    call :settings    
+    set OPTION=%1
 
 :start
     if "%OPTION%"=="" (
@@ -31,12 +36,30 @@ set OPTION=%1
     set OPTION=
     goto :start
 
+:init_colors 
+    rem red *;91
+    rem green *;92
+    rem yellow *;93
+    rem blue *;94
+    rem pink *;95
+    rem light blue *;96
+    rem purple *;97
+    rem black background 40;*
+    rem dark yellow background 43;*
+    rem purple background 45;*
+    rem blue background 44;*
+    set CL_HEADER=43;93
+    set CL_COMMAND=40;96
+    set CL_QUESTION=45;97
+    set CL_INFO=44;93
+    goto :eof  
+
 :eval_echo
     set input=%~1%
-    call set command=%%input:`="%%
+    rem replace all “ to "
+    call set command=%%input:“="%%
     call :color "%CL_COMMAND%" "%input%"
     call %command%
-
     goto :eof
 
 :ask_option
@@ -47,7 +70,6 @@ set OPTION=%1
     if "%CODE%"=="t" set OPTION=test
     if "%CODE%"=="r" set OPTION=run
     if "%CODE%"=="q" exit
-
     goto :eof
 
 :read_env
@@ -56,7 +78,6 @@ set OPTION=%1
         SET %%i
         call :color "%CL_INFO%" "%%i"
     )
-
     goto :eof
 
 :print_color
@@ -66,7 +87,6 @@ set OPTION=%1
          call :color "%CL_INFO%" "%%s"
     )
     del /Q %TOOLS%\out
-
     goto :eof
 
 :color
@@ -74,18 +94,15 @@ set OPTION=%1
     set message=%~2%
     echo [%color%m%message%[0m
     echo.
-
     goto :eof
 
 :ask
     call :color "%CL_QUESTION%" "Press any key to continue"
     pause >nul
-
     goto :eof
 
 :sep
     call :color "%CL_COMMAND%" "---------------------------------------------------------------------------------------"
-
     goto :eof
 
 :download_file
@@ -94,7 +111,6 @@ set OPTION=%1
     call :color "%CL_COMMAND%" "Downloading '%url%'"
     call :color "%CL_COMMAND%" "       into '%file%'"
     powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('%url%','%file%') }"
-
     goto :eof
 
 :install
@@ -115,96 +131,40 @@ set OPTION=%1
         call :eval_echo "rename %TOOLS%\..\%FOLDER% .%DEST%"
     )
     call :eval_echo "cd %ROOT%"
-
     goto :eof
 
 :settings
     call :color "%CL_HEADER%" "Setup variables..."
-
     call :read_env
-
     set ROOT=%CD%
-
     if "%SKIP_TESTS%"==""  ( set SKIP_TESTS=true)
-
     set CODE_PAGE=65001
     chcp %CODE_PAGE%
-
     set TOOLS=%ROOT%\.tools
     set ARCH=%TOOLS%\7z\7za.exe
-
     rem Set to true if you want to ignore go installation on the system
     if "%INSTALL_LOCALLY%"==""     ( set INSTALL_LOCALLY=true)
-
-    if "%INSTALL_LOCALLY%"=="true" ( set JAVA_HOME=)
-    if "%INSTALL_LOCALLY%"=="true" ( set MAVEN_HOME=)
-
-    if "%JAVA_HOME%"==""    ( set NO_JAVA=true)
-    if "%NO_JAVA%"=="true"  ( set JAVA_HOME=%ROOT%\.jdk)
-    if "%NO_JAVA%"=="true"  ( set PATH=%JAVA_HOME%\bin;%PATH%)
-
-    if "%MAVEN_HOME%"==""   ( set NO_MAVEN=true)
-    if "%NO_MAVEN%"=="true" ( set MAVEN_HOME=%ROOT%\.mvn)
-    if "%NO_MAVEN%"=="true" ( set MAVEN_USER_HOME=%MAVEN_HOME%)
-    if "%NO_MAVEN%"=="true" ( set MAVEN_OPTS=-Dmaven.repo.local=%MAVEN_HOME%\repository)
-
-    set MVNW=%ROOT%\mvnw
-    set MVNW_VERBOSE=false
-    set JAVA=%JAVA_HOME%\bin\java
-
-    echo Language enviromnent variables
-    call :color "%CL_INFO%" "PATH=%PATH%"
-    call :color "%CL_INFO%" "JAVA_HOME=%JAVA_HOME%"
-    call :color "%CL_INFO%" "MAVEN_HOME=%MAVEN_HOME%"
-    call :color "%CL_INFO%" "MAVEN_OPTS=%MAVEN_OPTS%"
-    call :color "%CL_INFO%" "MAVEN_USER_HOME=%MAVEN_USER_HOME%"
-    call :color "%CL_INFO%" "MVNW_VERBOSE=%MVNW_VERBOSE%"
-
-    set ARCH_URL=https://aka.ms/download-jdk/microsoft-jdk-11.0.11.9.1-windows-x64.zip
-    set ARCH_FOLDER=jdk-11.0.11+9
-
+    call stuff :settings
     goto :eof
 
 :download
     call :color "%CL_HEADER%" "Installing..."
-
-    if     "%INSTALL_LOCALLY%"=="true" call :install jdk %ARCH_URL% %ARCH_FOLDER%
+    if     "%INSTALL_LOCALLY%"=="true" call stuff :install
     if NOT "%INSTALL_LOCALLY%"=="true" call :color "%CL_INFO%" "The environment installed on the system is used"
-
-    call :print_color %MVNW% -v
-
+    call stuff :version
     goto :eof
 
 :build
     call :color "%CL_HEADER%" "Building client..."
-
-    call :print_color %MVNW% -v
-
-    rem install jar to maven local repo
-    call :eval_echo "%MVNW% clean install -DskipTests=%SKIP_TESTS%"
-
-    rem create executable jar
-    call :eval_echo "%MVNW% compile assembly:single -Pjar-with-dependencies -DskipTests=%SKIP_TESTS%"
-
-    call :eval_echo "copy %ROOT%\target\client-exec.jar %ROOT%\"
-    call :color "%CL_INFO%" "The executable file is located here: %ROOT%\client-exec.jar"
-
+    call stuff :build
     goto :eof
 
 :test
     call :color "%CL_HEADER%" "Starting tests..."
-
-    call :eval_echo "%MVNW% clean test"
-
+    call stuff :test
     goto :eof
 
 :run
     call :color "%CL_HEADER%" "Running client..."
-
-    rem run jar
-    rem call :eval_echo "%JAVA% -Dfile.encoding=UTF-8 -jar %ROOT%\client-exec.jar `%GAME_TO_RUN%` `%SERVER_URL%`"
-
-    rem build & run (without jar)
-    call :eval_echo "%MVNW% clean compile exec:java -Dfile.encoding=UTF-8 -Dexec.mainClass=com.codenjoy.dojo.JavaRunner -Dexec.args=`%GAME_TO_RUN% %SERVER_URL%`"
-
+    call stuff :run
     goto :eof
