ea7c2d576b81a6e19f68780fe2d8b32e540c6588
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-12-17 11:11:44 +0200

    [clients][java][elements][build] Port script from batch to bash.

---
+++ elements.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+eval_echo() {
+    command=$1
+    color=94 # blue
+    echo "[${color}m$command[0m"
+    echo
+    eval $command
+}
+
+eval_echo "echo Generating elements for all clients..."
+
+eval_echo "BASE=$(pwd)/.."
+
+# to select all games run
+# eval_echo "GAMES=all'
+eval_echo "GAMES=a2048,battlecity,chess,clifford,collapse,expansion,fifteen,football,hex,icancode,japanese,kata,lemonade,lunolet,minesweeper,moebius,mollymage,pong,puzzlebox,quadro,quake2d,reversi,rubicscube,sample,sample-text,selfdefense,snake,snakebattle,sokoban,spacerace,startandjump,sudoku,tetris,vacuum,verland,xonix"
+eval_echo "CLIENTS=md,md_header,md_footer,cpp,go,js,php,python,csharp"
+
+# TODO excitebike(rename elements,add info)
+# TODO icancode(js)(add info)
+# TODO expansion(add info)
+
+eval_echo "./mvnw clean compile exec:java -Dfile.encoding=UTF-8 -Dexec.mainClass=com.codenjoy.dojo.client.generator.ElementGeneratorRunner -Dexec.args='$BASE $GAMES $CLIENTS'"
+
+echo Press Enter to continue
+read
