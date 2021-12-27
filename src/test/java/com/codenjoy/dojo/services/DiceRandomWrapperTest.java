6d52ccc23f1c24cc03f896f29b86f1c89545648d
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-12-28 01:08:34 +0200

    [engine][verland] Added new DiceRandomWrapper to shuffle heroes spots on start.

---
+++ src/test/java/com/codenjoy/dojo/services/DiceRandomWrapperTest.java
@@ -0,0 +1,54 @@
+package com.codenjoy.dojo.services;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2021 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DiceRandomWrapperTest {
+
+    @Test
+    public void test() {
+        // given
+        Dice dice = mock(Dice.class);
+        Random random = new DiceRandomWrapper(dice);
+        when(dice.next(anyInt())).thenReturn(24);
+
+        // when then
+        assertEquals(-1155484576, random.nextInt());
+        assertEquals(-723955400, random.nextInt());
+
+        assertEquals(false, random.nextBoolean());
+        assertEquals(true, random.nextBoolean());
+
+        assertEquals(-6688467811848818630L, random.nextLong());
+        assertEquals(-8292973307042192125L, random.nextLong());
+    }
+
+}
