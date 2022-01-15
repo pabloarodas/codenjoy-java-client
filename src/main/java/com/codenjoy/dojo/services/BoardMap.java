f2a5633b7716fc8ad552bc76b3bf4da013535010
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-15 16:39:06 +0200

    [games][all][engine][clients][java] Refactoring near LengthToXY. Extracted LengthToXY.Map -> BoardMap.

--- src/main/java/com/codenjoy/dojo/services/BoardMap.java
+++ src/main/java/com/codenjoy/dojo/services/BoardMap.java
@@ -10,38 +10,56 @@ package com.codenjoy.dojo.services;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
+public class BoardMap { // TODO test me
 
-import static com.codenjoy.dojo.services.PointImpl.pt;
-
-public class LengthToXY {
-
+    private LengthToXY xy;
+    private char[] map;
     private int size;
 
-    public LengthToXY(int size) {
-        this.size = size;
+    public BoardMap(String map) {
+        this.map = map.toCharArray();
+        size = (int) Math.sqrt(map.length());
+        xy = new LengthToXY(size);
     }
 
-    public Point point(int length) {
-        if (length == -1) {
-            return null;
-        }
-        return pt(length % size, size - 1 - length / size);
+    public BoardMap(int size) {
+        map = new char[size * size];
+        xy = new LengthToXY(size);
     }
 
-    public int length(int x, int y) {
-        return (size - 1 - y) * size + x;
+    public int size() {
+        return size;
     }
 
+    public char getAt(int x, int y) {
+        int length = xy.length(x, y);
+        return map[length];
+    }
+
+    public char setAt(int x, int y, char ch) {
+        int length = xy.length(x, y);
+        char old = map[length];
+        map[length] = ch;
+        return old;
+    }
+
+    public boolean isOutOf(int x, int y) {
+        return Point.isOutOf(x, y, size);
+    }
+
+    public String map() {
+        return new String(map);
+    }
 }
